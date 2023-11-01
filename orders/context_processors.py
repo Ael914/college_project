66362370a7d83be029726aff70a37f3527b2465f
Ael914 <dla3lon@gmail.com>
2023-11-01 20:51:50 +0700

    Исправил отображение списка заказов

--- orders/context_processors.py
+++ orders/context_processors.py
@@ -2,5 +2,8 @@ from django.http import HttpRequest
 
 
 def have_orders(request: HttpRequest):
-    orders = "orders" in request.session
+    if "orders" in request.session:
+        orders = True if len(request.session["orders"]) > 0 else False
+    else:
+        orders = False
     return {"have_orders": orders}
